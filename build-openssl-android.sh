3f4800042d3e7cf0b2269a6bfde878c10684014b
Gregory <greghojob@gmail.com>
2018-09-07 12:11:10 +0800

    Add files via upload

---
+++ build-openssl-android.sh
@@ -0,0 +1,55 @@
+ANDROID_ARCH=${1:-"arch-arm"}
+OPENSSL_VERSION=${2:-"1.0.1i"}
+OPENSSL_TARBALL=openssl-${OPENSSL_VERSION}.tar.gz
+OPENSSL_DIR=openssl-${OPENSSL_VERSION}
+OPENSSL_BUILD_LOG=openssl-${OPENSSL_VERSION}.log
+
+# Download setenv_android.sh
+if [ ! -e setenv-android.sh ]; then 
+	echo "Downloading setenv_android.sh..."
+	curl -# -o setenv-android.sh http://wiki.openssl.org/images/7/70/Setenv-android.sh
+	chmod a+x setenv-android.sh
+fi
+
+# Download openssl source
+if [ ! -e ${OPENSSL_TARBALL} ]; then
+	echo "Downloading openssl-${OPENSSL_VERSION}.tar.gz..."
+	curl -# -O https://www.openssl.org/source/${OPENSSL_TARBALL}
+fi
+
+# Verify the source file
+if [ ! -e ${OPENSSL_TARBALL}.sha1 ]; then
+	echo -n "Verifying...	"
+	curl -o ${OPENSSL_TARBALL}.sha1 -s https://www.openssl.org/source/${OPENSSL_TARBALL}.sha1
+	CHECKSUM=`cat ${OPENSSL_TARBALL}.sha1`
+	ACTUAL=`shasum ${OPENSSL_TARBALL} | awk '{ print \$1 }'`
+	if [ "x$ACTUAL" == "x$CHECKSUM" ]; then
+		echo "OK"
+	else
+		echo "FAIL"
+		rm -f ${OPENSSL_TARBALL}
+		rm -f ${OPENSSL_TARBALL}.sha1
+		return 1
+	fi
+fi
+
+# Untar the file
+if [ ! -e ${OPENSSL_DIR} ]; then
+	tar zxf ${OPENSSL_TARBALL}
+fi
+
+# Setup the environment
+echo "Setup envrionment for ${ANDROID_ARCH}"
+. ./setenv-android.sh
+
+# Build
+echo "Compiling..."
+cd ${OPENSSL_DIR}
+perl -pi -e 's/install: all install_docs install_sw/install: install_docs install_sw/g' Makefile.org
+./config shared -no-ssl2 -no-ssl3 -no-comp -no-hw -no-engine --openssldir=/Users/$USER/GitHub/OpenSSL/lib/{$ANDROID_ARCH}/${ANDROID_API} > ../${OPENSSL_BUILD_LOG}
+make depend >> ../${OPENSSL_BUILD_LOG} 
+make all >> ../${OPENSSL_BUILD_LOG}
+
+# Installing
+echo "Installing..."
+sudo -E make install CC=$ANDROID_TOOLCHAIN/arm-linux-androideabi-gcc RANLIB=$ANDROID_TOOLCHAIN/arm-linux-androideabi-ranlib  >> ../${OPENSSL_BUILD_LOG}
